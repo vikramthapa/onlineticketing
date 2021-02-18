9e7a0b50d2cfa9e71b109fa8518102d74016704c
Vikram Thapa <the.axe772000@gmail.com>
2021-02-18 22:13:49 +0545

    Project started with boilerplate codes generated using cli tools

---
+++ OnlineTicketServer/src/main/java/com/onlineticket/OnlineTicketServer/OnlineTicketServerApplication.java
@@ -0,0 +1,13 @@
+package com.onlineticket.OnlineTicketServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTicketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTicketServerApplication.class, args);
+	}
+
+}
---
+++ OnlineTicketServer/src/test/java/com/onlineticket/OnlineTicketServer/OnlineTicketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineticket.OnlineTicketServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTicketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
